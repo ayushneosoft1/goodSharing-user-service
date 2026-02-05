aa97284bb7e487ca251af5316a5bcba40ace94d5
nileshMaurya123 <nilesh7874@gmail.com>
2026-02-05 15:43:31 +0530

    getAllUsers Query Added

--- resolvers.js
+++ resolvers.js
@@ -3,11 +3,18 @@ import bcrypt from "bcryptjs";
 import { pool } from "./db.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
 
 export const resolvers = {
   Query: {
     async getUserDetails(parent, args, context) {
+      console.log(" Context object:", context);
+      console.log(" Context user:", context.user);
+
       if (!context.user) {
+        console.log(" Unauthorized: No user in context");
         return {
           status: 401,
           statusMessage: "Unauthorized",
@@ -15,29 +22,67 @@ export const resolvers = {
         };
       }
 
+      const userId = Number(context.user.id);
+
       const { rows } = await pool.query(
-        "SELECT id, email, name FROM users WHERE id = $1",
-        [context.user.id]
+        "SELECT id, email, first_name,last_name FROM users WHERE id = $1",
+        [userId],
       );
+
+      if (!rows.length) {
+        return {
+          status: 404,
+          statusMessage: "User not found",
+          data: null,
+        };
+      }
 
       return {
         status: 200,
         statusMessage: "User details fetched successfully",
-        data: rows[0],
+        data: rows[0] || null,
       };
+    },
+
+    async getAllUserDetails(parent, args, context) {
+      console.log("context.user", context.user);
+      if (!context.user) {
+        return {
+          status: 401,
+          statusMessage: "Unauthorized",
+          data: null,
+        };
+      }
+      try {
+        const { rows } = await pool.query(
+          "SELECT id,email,first_name,last_name FROM users",
+        );
+        return {
+          status: 200,
+          statusMessage: "All users detailed fetched successfully",
+          data: rows,
+        };
+      } catch (error) {
+        console.error(error);
+        return {
+          status: 500,
+          statusMessage: "Internal server error",
+          data: null,
+        };
+      }
     },
   },
 
   Mutation: {
-    async signup(_, { email, password, name }) {
+    async signup(_, { email, password, first_name, last_name }) {
       const hashedPassword = await bcrypt.hash(password, 10);
 
       try {
         const { rows } = await pool.query(
-          `INSERT INTO users (email, password, name)
-         VALUES ($1, $2, $3)
-         RETURNING id, email, name`,
-          [email, hashedPassword, name]
+          `INSERT INTO users (email, password, first_name,last_name)
+         VALUES ($1, $2, $3,$4)
+         RETURNING id, email, first_name,last_name`,
+          [email, hashedPassword, first_name, last_name],
         );
 
         const user = rows[0];
@@ -76,7 +121,7 @@ export const resolvers = {
     async signin(_, { email, password }) {
       const { rows } = await pool.query(
         "SELECT * FROM users WHERE email = $1",
-        [email]
+        [email],
       );
 
       const user = rows[0];
@@ -108,9 +153,10 @@ export const resolvers = {
         data: {
           token,
           user: {
-            id: user.id,
-            email: user.email,
-            name: user.name,
+            id: rows[0].id,
+            email: rows[0].email,
+            first_name: rows[0].first_name,
+            last_name: rows[0].last_name,
           },
         },
       };
@@ -120,8 +166,8 @@ export const resolvers = {
   User: {
     async __resolveReference(ref) {
       const { rows } = await pool.query(
-        "SELECT id, email, name FROM users WHERE id = $1",
-        [ref.id]
+        "SELECT id, email, first_name,last_name FROM users WHERE id = $1",
+        [ref.id],
       );
       return rows[0];
     },
